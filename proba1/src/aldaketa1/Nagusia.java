cd0e07e53b4da835bd04027a17aa08df101a7fa6
iabd <iabd@fedora>
2026-01-19 17:02:47 +0100

    Aldaketak Nagusia klasean adar berritik

--- proba1/src/aldaketa1/Nagusia.java
+++ proba1/src/aldaketa1/Nagusia.java
@@ -1,5 +1,10 @@
 package aldaketa1;
 
 public class Nagusia {
-
+	public static void main (String [] args) {
+		System. out .println ("Hola mundo java-git");		
+		System. out .println ("Hola mundo java-git");		
+		System. out .println ("Agur");		
+		System. out .println ("Git probak. Aldaketa gehitu da");	
+	}
 }
